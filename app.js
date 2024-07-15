55cd09e19e1d0053ad1fd06094c90d6f8b84e3b7
AtharvaKamble <atharvadkamble00@gmail.com>
2024-07-14 23:18:40 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function run(textArea) {
+  const code = textArea.value;
+
+  console.log(code);
+
+  // make a server request to what ever this language is, get the output and show it in the display
+}
